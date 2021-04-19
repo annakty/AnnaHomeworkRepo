379338a57a3ceb5cdf91509ca792a5cebcdda472
anya <agontsova@gmail.com>
2021-04-18 23:18:58 -0400

    homework #12

---
+++ MyHomeWork/HomeWork12/HomeWork12_TaskDescription.java
@@ -0,0 +1,33 @@
+package MyHomeWork.HomeWork12;
+
+public class HomeWork12_TaskDescription {
+
+    /**
+     * Create a Restaurant class:
+     *
+     * name
+     * maxGuestCapacity
+     * currentGuestCount
+     * totalGuestCount
+     *
+     * 1. Constructor that allows user to start a restaurant by passing restaurant name amd max capacity.
+     *      data from user to store name and maxGuestCapacity
+     *
+     * 2. Create a method that tells user if the X numbers of seats are available
+     *      parameter: int
+     *      return type: boolean
+     *
+     * 3. Create a method that seats party
+     *      parameter: int
+     *
+     * 4. Create a method that remove party
+     *      parameter: int
+     *
+     * 5. Create a method that prints the restaurant summary:
+     *      Name:
+     *      Max capacity:
+     *      Current guest count:
+     *      Total guests served so far:
+     *
+     */
+}
